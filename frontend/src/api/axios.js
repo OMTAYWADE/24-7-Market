3f7d883e157fc163d603613b5d01981fd90fb7ff
omtaywade9@gmail.com <omtaywade9@gmail.com>
2026-06-19 21:03:43 +0530

    update production api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://two4-7-market-backend.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
